0253fe93d6e1a224bbb6519931bc345aea86974b
Duane Curlee <duane.curlee@gmail.com>
2019-04-23 20:56:15 -0400

    Now has settings file: .pyarc.txt

--- pyarc-cli.py
+++ pyarc-cli.py
@@ -2,9 +2,13 @@
 
 import sys, os, zipfile, datetime
 
+something_found = False
+skip_hidden_dirs = True
+skip_empty_dirs = True
+skip_pyarc_cli_files = True
+
 my_now = datetime.datetime.now()
 my_fname = 'pyarc-cli-' + my_now.strftime('%Y-%m-%d-%H-%M-%S') + '.zip'
-something_found = False
 
 if len(sys.argv) < 2:
     print('Exiting, command-line folders are required. No folders archived.')
@@ -15,11 +19,18 @@ else:
         if os.path.isdir(arg):
             something_found = True
             for root, dirs, files in os.walk(arg):
-                dirs[:]  = [d for d in dirs  if not d.startswith('.')]
-                files[:] = [f for f in files if not f.startswith('pyarc-cli-')]
+                if skip_hidden_dirs == True:
+                    dirs[:]  = [d for d in dirs  if not d.startswith('.')]
+                if skip_pyarc_cli_files == True:
+                    files[:] = [f for f in files if not f.startswith('pyarc-cli-')]
                 for name in files:
                     print('Archiving: ' + os.path.join(root, name))
                     my_zip.write(os.path.join(root, name), compress_type=zipfile.ZIP_DEFLATED)
+                if skip_empty_dirs == False:
+                    for name in dirs:
+                        my_zip.write(os.path.join(root, name), compress_type=zipfile.ZIP_DEFLATED)
+        elif os.path.isfile(arg):
+            print('Skipping, not a folder: ' + arg)
         else:
             print('Skipping, folder not found: ' + arg)
 
